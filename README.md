f3e44ad68a61ad3d1961842d730cead7078d031a
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-06-26 19:33:50 +0200

    section 5 complete, readme file added

---
+++ README.md
@@ -0,0 +1,6 @@
+# Udemy React course
+This is my material from the Udemy course
+[React - The Complete Guide (incl Hooks, React Router, Redux)](https://www.udemy.com/course/react-the-complete-guide-incl-redux/).
+
+The assignments are my own solutions to the assignments from the course, the rest is just coding along
+with the videos and thus should not be considered my own work.
--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import classes from "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -35,6 +35,8 @@ class App extends Component {
   render() {
     // Inline styling
     let persons = null;
+    let buttonClass = ""; // empty default
+
     if (this.state.showPersons) {
       persons = (
         <div>
@@ -48,22 +50,25 @@ class App extends Component {
           })}
         </div>
       );
+
+      buttonClass = classes.Red;
     }
 
-    const classes = [ ];
+    const assignedClasses = [ ];
     if (this.state.persons.length <= 2) {
-      classes.push("red");
+      assignedClasses.push(classes.red);
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push("bold");
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={ classes.App }>
         <h1>Hi, I am a React App</h1>
-        <p className={ classes.join(" ") }>This is really working!</p>
+        <p className={ assignedClasses.join(" ") }>This is really working!</p>
         <button
+          className={ buttonClass }
           onClick={ this.togglePersonsHandler }>Toggle persons
         </button>
         { persons }
