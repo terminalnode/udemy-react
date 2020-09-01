01e0cda83e6353b287278da3bcd5dfc62196d8d7
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-09-01 22:28:36 +0200

    section 8: add burgerbuilder controls, more-button

--- hamburger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ hamburger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,22 +2,49 @@ import React, { Component } from "react";
 
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 1,
-      meat: 1,
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
     }
-  }
+  };
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = oldCount + 1;
+
+    const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+    this.setState({
+      totalPrice: newPrice,
+      ingredients: updatedIngredients
+    })
+  };
+
+  removeIngredientHandler = (type) => {
+    console.log("Please implement me!");
+  };
 
   render() {
     return (
       <Aux>
         <Burger ingredients={ this.state.ingredients } />
-        <div>Build Controls</div>
+        <BuildControls
+          ingredientAdded={ this.addIngredientHandler } />
       </Aux>
     );
   }
